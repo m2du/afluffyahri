3a1974b9a8181332ba430b96b71ffc3bbdd10cf8
Michael Du <michaeldu.work@gmail.com>
2019-09-25 19:46:03 -0700

    Refactor media links into its own component

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import TwitchIcon from './icons/twitch_icon.png';
-import TwitterIcon from './icons/twitter_icon.png';
-import TumblrIcon from './icons/tumblr_icon.png';
-import YoutubeIcon from './icons/youtube_icon.png';
-import StreamlabsIcon from './icons/streamlabs_icon.png';
+import MediaBar from '../mediabar/mediabar';
 import './about.css';
 
 export default (props) => {
@@ -24,13 +20,7 @@ export default (props) => {
                 <pre>&#9;</pre>I have been streaming ever since January 2017. My goal as a Streamer is to grow a nice playful supportive community, and one day become partnered. 
                 I stream many different kind of games, from FPS to MOBAs to MMORPG. Anyone is welcomed to come and watch, I love to interact and play with my viewers.
             </p>
-            <div className='media-container'>
-                <a href='https://www.twitch.tv/afluffyahri' className='media-link'><img className='media-icon' src={TwitchIcon} alt='twitch.tv' /> /afluffyahri</a><pre>&emsp;&emsp;&emsp;&emsp;</pre>
-                <a href='https://twitter.com/AFluffyAhri' className='media-link'><img className='media-icon' src={TwitterIcon} alt='twitter.com' /> @AFluffyAhri</a><pre>&emsp;&emsp;&emsp;&emsp;</pre>
-                <a href='https://theforevernub.tumblr.com/' className='media-link'><img className='media-icon' src={TumblrIcon} alt='tumblr.com' /> theforevernub</a><pre>&emsp;&emsp;&emsp;&emsp;</pre>
-                <a href='https://www.youtube.com/user/TheForeverNub' className='media-link'><img className='media-icon' src={YoutubeIcon} alt='youtube.com' /> TheForeverNub</a><pre>&emsp;&emsp;&emsp;&emsp;</pre>
-                <a href='https://streamlabs.com/afluffyahri/merch' className='media-link'><img className='media-icon' src={StreamlabsIcon} alt='streamlabs.com' /> Merch</a>
-            </div>
+            <MediaBar />
         </div>
     )
 }
---
+++ src/components/mediabar/mediabar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import TwitchIcon from './icons/twitch_icon.png';
+import TwitterIcon from './icons/twitter_icon.png';
+import TumblrIcon from './icons/tumblr_icon.png';
+import YoutubeIcon from './icons/youtube_icon.png';
+import StreamlabsIcon from './icons/streamlabs_icon.png';
+import './mediabar.css';
+
+export default (props) => {
+    return (
+        <div className='media-container'>
+            <a href='https://www.twitch.tv/afluffyahri' className='media-link'><img className='media-icon' src={TwitchIcon} alt='twitch.tv' /> /afluffyahri</a><pre>&emsp;&emsp;&emsp;&emsp;</pre>
+            <a href='https://twitter.com/AFluffyAhri' className='media-link'><img className='media-icon' src={TwitterIcon} alt='twitter.com' /> @AFluffyAhri</a><pre>&emsp;&emsp;&emsp;&emsp;</pre>
+            <a href='https://theforevernub.tumblr.com/' className='media-link'><img className='media-icon' src={TumblrIcon} alt='tumblr.com' /> theforevernub</a><pre>&emsp;&emsp;&emsp;&emsp;</pre>
+            <a href='https://www.youtube.com/user/TheForeverNub' className='media-link'><img className='media-icon' src={YoutubeIcon} alt='youtube.com' /> TheForeverNub</a><pre>&emsp;&emsp;&emsp;&emsp;</pre>
+            <a href='https://streamlabs.com/afluffyahri/merch' className='media-link'><img className='media-icon' src={StreamlabsIcon} alt='streamlabs.com' /> Merch</a>
+        </div>
+    )
+}
