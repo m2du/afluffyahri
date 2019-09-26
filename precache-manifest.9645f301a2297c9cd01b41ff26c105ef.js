41e591420764544109fedf5cced9854af687aee4
Michael Du <michaeldu.work@gmail.com>
2019-09-25 20:26:31 -0700

    Updates

---
+++ precache-manifest.9645f301a2297c9cd01b41ff26c105ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f215a6eefa979f6bb5ea27dbb5bef0",
+    "url": "/afluffyahri/index.html"
+  },
+  {
+    "revision": "a884df6445548e28b194",
+    "url": "/afluffyahri/static/css/main.3b520f10.chunk.css"
+  },
+  {
+    "revision": "bf5473cedf6916a04c7a",
+    "url": "/afluffyahri/static/js/2.8c938852.chunk.js"
+  },
+  {
+    "revision": "a884df6445548e28b194",
+    "url": "/afluffyahri/static/js/main.2562e342.chunk.js"
+  },
+  {
+    "revision": "df0a467c74463477020f",
+    "url": "/afluffyahri/static/js/runtime-main.90aa1a0c.js"
+  },
+  {
+    "revision": "d03513060bcd52bd8e49dbe828710f07",
+    "url": "/afluffyahri/static/media/streamlabs_icon.d0351306.png"
+  }
+]);
