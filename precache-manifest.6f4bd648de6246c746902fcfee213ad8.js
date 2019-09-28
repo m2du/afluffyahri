5c3eed22430301cc4f421a83d1f5d0ceb8420108
Michael Du <michaeldu.work@gmail.com>
2019-09-27 20:27:57 -0700

    Updates

---
+++ precache-manifest.6f4bd648de6246c746902fcfee213ad8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8ea8e8f5a8d79e0d27d1961bcd9c78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a3548c1808a81b6e05a",
+    "url": "/static/css/main.2fcd1361.chunk.css"
+  },
+  {
+    "revision": "a0c3f63623aea41d0d45",
+    "url": "/static/js/2.00793e02.chunk.js"
+  },
+  {
+    "revision": "0a3548c1808a81b6e05a",
+    "url": "/static/js/main.7a5f6b72.chunk.js"
+  },
+  {
+    "revision": "8dcd1d9c814c266f37bf",
+    "url": "/static/js/runtime-main.8ee78844.js"
+  },
+  {
+    "revision": "428f4e43a7f7908eaf80bbf6774b0dc9",
+    "url": "/static/media/Banner.428f4e43.png"
+  },
+  {
+    "revision": "9ce152c5c499178d35dcd2413cfe06a7",
+    "url": "/static/media/scroll.9ce152c5.png"
+  },
+  {
+    "revision": "d03513060bcd52bd8e49dbe828710f07",
+    "url": "/static/media/streamlabs_icon.d0351306.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/afluffyahri/precache-manifest.a9a938416ce5fe43dd097904b44a46b1.js"
+  "/precache-manifest.6f4bd648de6246c746902fcfee213ad8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/afluffyahri/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
